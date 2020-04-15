398a200d3bca138d66552eb6042f324a49528bfb
Erfanx <irfansyed479@gmail.com>
2020-04-15 17:29:27 +0600

    Add User profile

---
+++ profile.php
@@ -0,0 +1,72 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html >
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Home | SNTL Airlines</title>
+<link rel="icon" href="img/icon.png">
+<link href="styles.css" type="text/css" rel="stylesheet" />
+<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+<div id="background_contain">
+<div id="menubar-id">
+        <div class="menubar-class">
+        <a href="index.php"><img id="menu_logo" src="img/logo.png"/></a>  
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="status.php">Flight Status</a></li>
+                <li><a href="reservation.html">Reservations</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
+				<li>
+				<?php  if (isset($_SESSION['username'])) : ?>
+						<a>Welcome <a style="text-transform: uppercase;"  href="profile.php"><strong><?php echo $_SESSION['username']; ?></strong></a></a>
+						 <a style="color: white;">|</a><a href="index.php?logout='1'" style="color: #fe28a2;"> Logout</a>
+						 
+					<?php endif ?>
+				</li>
+				
+            </ul>
+        </div>
+        
+        <div id="menubar_space">
+        
+        </div>
+        
+
+        
+        <div id="login_space">
+        
+        </div>
+       
+        <div class="quick_book">
+        	<h3 id="booking_header">Profile</h3>
+        	<a>User ID: <strong><?php echo $_SESSION['id']; ?></strong></a><br>
+			<a>Name: <strong><?php echo $_SESSION['username']; ?></strong></a><br>
+			<a>Email: <strong><?php echo $_SESSION['email']; ?></strong></a>
+		</div>
+        
+        </div>
+        
+</div>       
+<div class="footer">
+  <p>CSE311 | This website is made with &#128147; by Syed, Nafis, Tamanna & Lamia</p>
+</div>
+</body>
+
+</html>

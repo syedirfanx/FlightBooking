1bda9e6aacfe29f064e2469ead415462ed936af5
Erfanx <irfansyed479@gmail.com>
2020-04-16 14:22:35 +0600

    Resolve searchModel.php

--- searchModel.php
+++ searchModel.php
@@ -1,11 +1,9 @@
 <?php
-// Author: Ryan Nyborg
-//
+
 class DatabaseAdaptor {
-	// The instance variable used in every one of the functions in class DatbaseAdaptor
+	
 	private $DB;
-	// Make a connection to an existing data based named 'imdb_small' that has
-	// table . In this assignment you will also need a new table named 'users'
+
 	public function __construct() {
 		$db = 'mysql:dbname=bookaflight;host=127.0.0.1';
 		$user = 'root';
@@ -20,7 +18,6 @@ class DatabaseAdaptor {
 		}
 	}
 
-	// Return all flights in search as an associative array.
 	public function getFlights($origin, $destination, $depart, $passengers) {
 		$stmt = $this->DB->prepare ("SELECT * FROM flights WHERE origin = :origin AND destination = :destination 
 				AND depart_day = :depart_day AND seats_available >= :seats_available");
@@ -53,11 +50,11 @@ class DatabaseAdaptor {
 		$sql = $sql."values('$id', '$totalCost', '$tripType', '$depart_id', '$return_id', '$passengers')";
 		if($this->DB->query($sql)){
 			echo "<script>alert(\"Reservation created successfully.\");</script>";
-			echo "<script> document.location.href='reservation_login.php';</script>";
+			echo "<script> document.location.href='reservation.php';</script>";
 			exit;
 		}else{
 			echo "<script>alert(\"Reservation failed.\");</script>";
-			echo "<script> document.location.href='Home_login.php';</script>";
+			echo "<script> document.location.href='index.php';</script>";
 			exit;
 		}
 	}
@@ -90,26 +87,17 @@ class DatabaseAdaptor {
 		
 		if($this->DB->query($sql)){
 			echo "<script>alert(\"Reservation removed successfully.\");</script>";
-			echo "<script> document.location.href='reservation_login.php';</script>";
+			echo "<script> document.location.href='reservation.php';</script>";
 			exit;
 		}else{
 			echo "<script>alert(\"Reservation failed.\");</script>";
-			echo "<script> document.location.href='Home_login.php';</script>";
+			echo "<script> document.location.href='index.php';</script>";
 			exit;
 		}
 	}
-	
-	
-	/* SELECT reservation.memberSeq, flights.origin, flights.destination, reservation.totalCost, reservation.passengers,
-	 reservation.tripType FROM reservation JOIN flights ON reservation.depart_id = flights.flight_id WHERE reservation.id = "rnyborg";*/
 
-} // End class DatabaseAdaptor
+}
 
-// Testing code that should not be run when a part of MVC
 $theDBA = new DatabaseAdaptor ();
-//$theDBA->createReservation("rnyborg", 200.00, "ROUND", 156, 162, 3);
-//$theDBA->deleteReservation(1);
-// $arr = $theDBA->getDepartDetails(5);
-// print_r($arr);
 
 ?>
